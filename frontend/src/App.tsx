374b4aca44a46781f3f011df396ce55eac56538a
Loganbh0 <loganbrianhanson@gmail.com>
2026-03-17 14:17:08 -0600

    Removed exit button with weather link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AppHeader, QuickExitButton } from "@/components/AppShell";
+import { AppHeader } from "@/components/AppShell";
 import Landing from "./pages/Landing";
 import Onboarding from "./pages/Onboarding";
 import Mentors from "./pages/Mentors";
@@ -36,7 +36,6 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
-        <QuickExitButton />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
